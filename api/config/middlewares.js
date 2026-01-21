08f07e8b5b768cc7c6b5adb4ed5aa20b90e67620
Nabin <sharmanabin.0910@gmail.com>
2026-01-21 23:06:48 +0530

    chore : cors updation

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,12 +1,21 @@
 module.exports = [
-  "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://your-vercel-app.vercel.app',
+      ],
+      methods: ['GET','POST','PUT','DELETE'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
