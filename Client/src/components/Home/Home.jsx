54d4dd764b4e1b7396fdb8618ecebc9596a5f944
Nabin <sharmanabin.0910@gmail.com>
2026-01-22 16:56:29 +0530

    Bug-fix : Fix Home.jsx to consume context only

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,27 +1,11 @@
-import { useEffect, useContext } from "react";
+import { useContext } from "react";
 import Banner from "./Banner/Banner";
 import Category from "../Category/Category";
 import Products from "../Products/Products";
-import { fetchDataFromApi } from "../../utils/api";
 import { Context } from "../../utils/context";
 
 const Home = () => {
-    const {
-        categories,
-        setCategories,
-        products,
-        setProducts,
-    } = useContext(Context);
-
-    useEffect(() => {
-        fetchDataFromApi("/categories?populate=*").then((res) => {
-            setCategories(res.data);
-        });
-
-        fetchDataFromApi("/products?populate=*").then((res) => {
-            setProducts(res.data);
-        });
-    }, [setProducts, setCategories]);
+    const { categories, products } = useContext(Context);
 
     return (
         <>
